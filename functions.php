856451ba89dc86abdf5984623bfd04eff1573c28
svennijhuis <sanijhuis@live.nl>
2023-05-03 14:46:29 +0200

    add custom post type

--- functions.php
+++ functions.php
@@ -3,14 +3,10 @@
 
 
 /**
-
- * Day Six theme functions and definitions
-
- * 
-
- * @package Day Six theme
-
- */
+* Day Six theme functions and definitions
+* 
+* @package Day Six theme
+*/
 
 
 /*
@@ -22,11 +18,12 @@
 | 
 |
 */
-function add_theme_scripts() {
-    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), '1.1', 'all');
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/script/index.js', array(), 1.1, true);
-  }
-  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
+function add_theme_scripts()
+{
+    wp_enqueue_style('styles', get_template_directory_uri() . '/style.css', array(), '1.1', 'all');
+    wp_enqueue_script('script', get_template_directory_uri() . '/script/index.js', array(), 1.1, true);
+}
+add_action('wp_enqueue_scripts', 'add_theme_scripts');
 /*
 |--------------------------------------------------------------------------
 | Back-end styles en scripts
@@ -37,10 +34,11 @@ function add_theme_scripts() {
 |
 */
 
-function load_custom_wp_admin_style(){
-    wp_enqueue_style( 'swiper',  'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
-    wp_enqueue_style( 'styles', get_template_directory_uri() . '/style.css', array(), '1.1', 'all');
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/script/index.js', array(), 1.1, true);
+function load_custom_wp_admin_style()
+{
+    wp_enqueue_style('swiper', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css');
+    wp_enqueue_style('styles', get_template_directory_uri() . '/style.css', array(), '1.1', 'all');
+    wp_enqueue_script('script', get_template_directory_uri() . '/script/index.js', array(), 1.1, true);
 }
 add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
 
@@ -54,17 +52,18 @@ add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
 |
 */
 
-function day_six_config(){
-    register_nav_menus (
+function day_six_config()
+{
+    register_nav_menus(
         array(
             'day_six_main_menu' => 'Main Menu'
         )
     );
-    add_theme_support( 'post-thumbnails' );
-    add_image_size( 'preview', 100, 100, array( 'center', 'center' ) );
+    add_theme_support('post-thumbnails');
+    add_image_size('preview', 100, 100, array('center', 'center'));
 }
 
-add_action( 'after_setup_theme', 'day_six_config', 0 );
+add_action('after_setup_theme', 'day_six_config', 0);
 
 
 
@@ -86,61 +85,62 @@ add_action( 'after_setup_theme', 'day_six_config', 0 );
 */
 add_filter('block_categories_all', function ($categories) {
 
-    array_unshift($categories,           
-    [
-        'slug'  => 'styling',
-        'title' => 'styling',
-        'icon'  => null
-    ],  
-    [
-        'slug'  => 'hero',
-        'title' => 'hero',
-        'icon'  => null
-    ],
+    array_unshift(
+        $categories,
         [
-        'slug'  => 'paginablokken',
-        'title' => 'pagina blokken',
-        'icon'  => null
-    ],
-    [
-        'slug'  => 'containers',
-        'title' => 'containers',
-        'icon'  => null
-    ],
-    [
-        'slug'  => 'blokken',
-        'title' => 'blokken',
-        'icon'  => null
-    ],
+            'slug' => 'styling',
+            'title' => 'styling',
+            'icon' => null
+        ],
+        [
+            'slug' => 'hero',
+            'title' => 'hero',
+            'icon' => null
+        ],
+        [
+            'slug' => 'paginablokken',
+            'title' => 'pagina blokken',
+            'icon' => null
+        ],
+        [
+            'slug' => 'containers',
+            'title' => 'containers',
+            'icon' => null
+        ],
+        [
+            'slug' => 'blokken',
+            'title' => 'blokken',
+            'icon' => null
+        ],
 
-    [
-        'slug'  => 'cards',
-        'title' => 'cards',
-        'icon'  => null
-    ],
-    [
-        'slug'  => 'navigatie',
-        'title' => 'navigatie',
-        'icon'  => null
-    ],
-    [
-        'slug'  => 'innerblocks',
-        'title' => 'inner blocks',
-        'icon'  => null
-    ],
-    [
-        'slug'  => 'elements',
-        'title' => 'elements',
-        'icon'  => null
-    ],
-    [
-        'slug'  => 'page',
-        'title' => 'page',
-        'icon'  => null
-    ],
-);
+        [
+            'slug' => 'cards',
+            'title' => 'cards',
+            'icon' => null
+        ],
+        [
+            'slug' => 'navigatie',
+            'title' => 'navigatie',
+            'icon' => null
+        ],
+        [
+            'slug' => 'innerblocks',
+            'title' => 'inner blocks',
+            'icon' => null
+        ],
+        [
+            'slug' => 'elements',
+            'title' => 'elements',
+            'icon' => null
+        ],
+        [
+            'slug' => 'page',
+            'title' => 'page',
+            'icon' => null
+        ],
+    );
 
-return $categories;
+    return $categories;
 }, 10, 1);
 
 
@@ -149,10 +149,10 @@ return $categories;
 | All allowed blocks
 |--------------------------------------------------------------------------
 */
-add_filter('allowed_block_types_all', function($allowed_blocks, $editor_context) {
+add_filter('allowed_block_types_all', function ($allowed_blocks, $editor_context) {
     $blocks = get_blocks();
-    $acf_blocks = []; 
-    foreach ($blocks as $block) { 
+    $acf_blocks = [];
+    foreach ($blocks as $block) {
         $acf_blocks[] = 'acf/' . $block;
     }
 
@@ -170,12 +170,13 @@ add_filter('allowed_block_types_all', function($allowed_blocks, $editor_context)
 | Register blocks
 |--------------------------------------------------------------------------
 */
-add_action( 'init', 'register_acf_blocks', 5 );
-function register_acf_blocks() {
+add_action('init', 'register_acf_blocks', 5);
+function register_acf_blocks()
+{
 
     $blocks = get_blocks();
     foreach ($blocks as $block) {
-            register_block_type( __DIR__ . '/blocks/'.$block );
+        register_block_type(__DIR__ . '/blocks/' . $block);
     }
 }
 
@@ -184,18 +185,19 @@ function register_acf_blocks() {
 | Get all blocks name from the folder name
 |--------------------------------------------------------------------------
 */
-function get_blocks() {
-	$theme   = wp_get_theme();
-	$blocks  = get_option( 'cwp_blocks' );
-	$version = get_option( 'cwp_blocks_version' );
-	if ( empty( $blocks ) || version_compare( $theme->get( 'Version' ), $version ) || ( function_exists( 'wp_get_environment_type' ) && 'production' !== wp_get_environment_type() ) ) {
-		$blocks = scandir( get_template_directory() . '/blocks/' );
-		$blocks = array_values( array_diff( $blocks, array( '..', '.', '.DS_Store', '_base-block' ) ) );
+function get_blocks()
+{
+    $theme = wp_get_theme();
+    $blocks = get_option('cwp_blocks');
+    $version = get_option('cwp_blocks_version');
+    if (empty($blocks) || version_compare($theme->get('Version'), $version) || (function_exists('wp_get_environment_type') && 'production' !== wp_get_environment_type())) {
+        $blocks = scandir(get_template_directory() . '/blocks/');
+        $blocks = array_values(array_diff($blocks, array('..', '.', '.DS_Store', '_base-block')));
 
-		update_option( 'cwp_blocks', $blocks );
-		update_option( 'cwp_blocks_version', $theme->get( 'Version' ) );
-	}
-	return $blocks;
+        update_option('cwp_blocks', $blocks);
+        update_option('cwp_blocks_version', $theme->get('Version'));
+    }
+    return $blocks;
 }
 
 
@@ -204,14 +206,15 @@ function get_blocks() {
 | Script for one block
 |--------------------------------------------------------------------------
 */
-function cwp_register_block_script() {
+function cwp_register_block_script()
+{
     $blocks = get_blocks();
     foreach ($blocks as $block) {
-        wp_register_script( $block, get_template_directory_uri() . '/blocks/'.$block.'/script.js' );
+        wp_register_script($block, get_template_directory_uri() . '/blocks/' . $block . '/script.js');
     }
 
 }
-add_action( 'init', 'cwp_register_block_script' );
+add_action('init', 'cwp_register_block_script');
 
 
 /*
@@ -245,3 +248,64 @@ add_filter('acf/settings/load_json', function ($paths) {
     $paths[] = get_stylesheet_directory() . '/acf-json';
     return $paths;
 });
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Custom post type events
+|--------------------------------------------------------------------------
+|
+| 
+| 
+|
+*/
+
+function create_event_post_type()
+{
+    // Labels for the post type
+    $labels = array(
+        'name' => __('Events'),
+        'singular_name' => __('Event'),
+        'menu_name' => __('Events'),
+        'add_new' => __('Add New'),
+        'add_new_item' => __('Add New Event'),
+        'edit_item' => __('Edit Event'),
+        'new_item' => __('New Event'),
+        'view_item' => __('View Event'),
+        'search_items' => __('Search Events'),
+        'not_found' => __('No events found'),
+        'not_found_in_trash' => __('No events found in Trash'),
+        'all_items' => __('All Events'),
+    );
+    // Options for the post type
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'event'),
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => 5,
+        'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author'),
+        'taxonomies' => array('category', 'post_tag'),
+        'menu_icon' => 'dashicons-calendar-alt',
+    );
+
+    // Register the post type
+    register_post_type('event', $args);
+
+}
+add_action('init', 'create_event_post_type');
+function add_event_rest_support()
+{
+    global $wp_post_types;
+    $wp_post_types['event']->show_in_rest = true;
+    $wp_post_types['event']->rest_base = 'events';
+    $wp_post_types['event']->rest_controller_class = 'WP_REST_Posts_Controller';
+}
+add_action('init', 'add_event_rest_support', 25);
