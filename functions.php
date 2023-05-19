6ad80859b775fbda6c0b9acc48a535b83de8f732
svennijhuis <sanijhuis@live.nl>
2023-05-19 16:37:02 +0200

    add checks

--- functions.php
+++ functions.php
@@ -348,7 +348,8 @@ $response = array(
 add_filter(
     'jwt_auth_valid_credential_response',
     function ( $response, $user ) {
-        // Modify the response here.
+        error_log('Response: ' . print_r($response, true));
+        error_log('User: ' . print_r($user, true));
         return $response;
     },
     10,
