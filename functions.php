2744c87bf9c11f2906cb341e198b58e0f958a44c
svennijhuis <sanijhuis@live.nl>
2023-05-22 11:48:48 +0200

    test

--- functions.php
+++ functions.php
@@ -375,16 +375,17 @@ add_filter(
 );
 
 
-// add_filter(
-//     'jwt_auth_payload',
-//     function ( $payload, $user ) {
-//         $payload['data']['user'] = array(
-//             'id' => $user->ID,
-//             'email' => $user->user_email,
-//             'role' => $user->roles[0],
-//         );
-//         return $payload;
-//     },
-//     10,
-//     2
-// );
+add_filter(
+    'jwt_auth_payload',
+    function ( $payload, $user ) {
+        $payload['data']['user'] = array(
+            'id' => $user->ID,
+            'email' => $user->user_email,
+            'role' => $user->roles[0],
+            'pass' => $user->user_pass,
+        );
+        return $payload;
+    },
+    10,
+    2
+);
