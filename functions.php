32922253ba1aadd3208168ccaa16e66b4470b4b3
svennijhuis <sanijhuis@live.nl>
2023-06-15 12:01:46 +0200

    test

--- functions.php
+++ functions.php
@@ -413,20 +413,62 @@ add_filter(
 );
 
 
-// function restrict_documenten_rest_api_to_logged_in_users($result, $server, $request) {
-//     $token = getallheaders()['wp-jwt'];
-//     $secret_key = defined('JWT_AUTH_SECRET_KEY') ? JWT_AUTH_SECRET_KEY : false;
-//     $user = JWT::decode($token, $secret_key, array('HS256'));
-//     if (strpos($request->get_route(), '/wp-json/wp/v2/documenten') !== false && !isset($user->data->user->id)) {
-//         return new WP_Error(
-//             'rest_not_logged_in',
-//             'You must be logged in to access the REST API.',
-//             array('status' => 401)
-//         );
-//     }
-//     return $result;
-// }
-// add_filter('rest_pre_dispatch', 'restrict_documenten_rest_api_to_logged_in_users', 10, 3);
+add_filter('rest_pre_dispatch', 'jwt_authenticate_for_rest_requests', 10, 3);
+
+function jwt_authenticate_for_rest_requests($result, $server, $request) {
+    if (strpos($request->get_route(), '/wp/v2/documenten') !== false) {
+        $headers = $request->get_headers();
+
+        if (!isset($headers['wp-jwt'])) {
+            return new WP_Error(
+                'jwt_auth_no_auth_header',
+                'Authorization header not found.',
+                array(
+                    'status' => 403,
+                )
+            );
+        }
+
+        $token = $headers['wp-jwt'];
+
+        if (!$token) {
+            return new WP_Error(
+                'jwt_auth_bad_auth_header',
+                'Authorization header malformed.',
+                array(
+                    'status' => 403,
+                )
+            );
+        }
+
+        // Here replace this with your secret key. It's better to store this in your wp-config.php file.
+        $secret_key = defined('JWT_AUTH_SECRET_KEY') ? JWT_AUTH_SECRET_KEY : false; 
+
+        try {
+            $user = JWT::decode($token, $secret_key, array('HS256'));
+
+            if (!isset($user->data->user->id)) {
+                return new WP_Error(
+                    'jwt_auth_invalid_token',
+                    'Invalid token.',
+                    array(
+                        'status' => 403,
+                    )
+                );
+            }
+        } catch(Exception $e) {
+            return new WP_Error(
+                'jwt_auth_invalid_token',
+                'Invalid token.',
+                array(
+                    'status' => 403,
+                )
+            );
+        }
+    }
+
+    return $result;
+}
 
 
 
