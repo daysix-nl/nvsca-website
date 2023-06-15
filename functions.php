2d0aadadd6d30eb2cb0656f3b8cda3675938291d
svennijhuis <sanijhuis@live.nl>
2023-06-15 11:47:04 +0200

    test

--- functions.php
+++ functions.php
@@ -414,37 +414,19 @@ add_filter(
 
 
 function restrict_documenten_rest_api_to_logged_in_users($result, $server, $request) {
-    if (strpos($request->get_route(), '/wp/v2/documenten') !== false) {
-        if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
-            return rest_ensure_response(new WP_Error(
-                'jwt_auth_no_auth_header',
-                'Authorization header not found.',
-                array('status' => 403)))
-                ->set_headers(array(
-                    'Access-Control-Allow-Origin' => '*', 
-                    'Access-Control-Allow-Headers' => 'wp-jwt'
-                ));
-        }
-
-        $token = getallheaders()['wp-jwt'];
-        $secret_key = defined('JWT_AUTH_SECRET_KEY') ? JWT_AUTH_SECRET_KEY : false;
-        $user = JWT::decode($token, $secret_key, array('HS256'));
-
-        if (!isset($user->data->user->id)) {
-            return rest_ensure_response(new WP_Error(
-                'jwt_auth_invalid_token',
-                'Invalid token.',
-                array('status' => 403)))
-                ->set_headers(array(
-                    'Access-Control-Allow-Origin' => '*', 
-                    'Access-Control-Allow-Headers' => 'wp-jwt'
-                ));
-        }
+    $token = getallheaders()['wp-jwt'];
+    $secret_key = defined('JWT_AUTH_SECRET_KEY') ? JWT_AUTH_SECRET_KEY : false;
+    $user = JWT::decode($token, $secret_key, array('HS256'));
+    if (strpos($request->get_route(), '/wp/v2/documenten') !== false && !isset($user->data->user->id)) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            'You must be logged in to access the REST API.',
+            array('status' => 401)
+        );
     }
-
     return $result;
 }
-
 add_filter('rest_pre_dispatch', 'restrict_documenten_rest_api_to_logged_in_users', 10, 3);
+
 
 
