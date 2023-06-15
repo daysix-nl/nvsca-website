e21cd45b18faba914132cb5e6387e1a87ce609f4
svennijhuis <sanijhuis@live.nl>
2023-06-15 11:07:19 +0200

    test

--- functions.php
+++ functions.php
@@ -413,14 +413,14 @@ add_filter(
 );
 
 
-// function restrict_documenten_rest_api_to_logged_in_users($result, $server, $request) {
-//     if (strpos($request->get_route(), '/wp/v2/documenten') !== false && !is_user_logged_in()) {
-//         return new WP_Error(
-//             'rest_not_logged_in',
-//             'You must be logged in to access the REST API.',
-//             array('status' => 401)
-//         );
-//     }
-//     return $result;
-// }
-// add_filter('rest_pre_dispatch', 'restrict_documenten_rest_api_to_logged_in_users', 10, 3);
+function restrict_documenten_rest_api_to_logged_in_users($result, $server, $request) {
+    if (strpos($request->get_route(), '/wp/v2/documenten') !== false && !is_user_logged_in()) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            'You must be logged in to access the REST API.',
+            array('status' => 401)
+        );
+    }
+    return $result;
+}
+add_filter('rest_pre_dispatch', 'restrict_documenten_rest_api_to_logged_in_users', 10, 3);
