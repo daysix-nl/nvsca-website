2eac777c8773f02f72165cbfe71b2db2c4ffa2a9
svennijhuis <sanijhuis@live.nl>
2023-06-16 11:33:40 +0200

    test

--- functions.php
+++ functions.php
@@ -465,15 +465,16 @@ function jwt_authenticate_for_rest_requests($result, $server, $request) {
                     )
                 );
             }
-        } catch(BeforeValidException $e) {
-            return new WP_Error(
-                'jwt_auth_expired_token',
-                'Expired token.',
-                array(
-                    'status' => 403,
-                )
-            );
         }
+        // } catch(BeforeValidException $e) {
+        //     return new WP_Error(
+        //         'jwt_auth_expired_token',
+        //         'Expired token.',
+        //         array(
+        //             'status' => 403,
+        //         )
+        //     );
+        // }
         // } catch(ExpiredException $e) {
         //     return new WP_Error(
         //         'jwt_auth_expired_token',
