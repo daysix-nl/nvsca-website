519fe3a66ff26196880b8efdc57e43a37c7d850a
svennijhuis <sanijhuis@live.nl>
2023-06-21 10:49:56 +0200

    test

--- functions.php
+++ functions.php
@@ -830,7 +830,7 @@ if( function_exists('acf_add_options_page') ) {
     acf_add_options_page(array(
         'page_title'    => 'Theme General Settings',
         'menu_title'    => 'Theme Settings',
-        'menu_slug'     => 'theme-general-settings',
+        'menu_slug'     => 'test',
         'capability'    => 'edit_posts',
         'redirect'      => false
     ));
@@ -838,7 +838,7 @@ if( function_exists('acf_add_options_page') ) {
     acf_add_options_sub_page(array(
         'page_title'    => 'Document categorieen',
         'menu_title'    => 'Categorieen',
-        'parent_slug'   => 'theme-general-settings',
+        'parent_slug'   => 'testtest',
     ));
 }
 
